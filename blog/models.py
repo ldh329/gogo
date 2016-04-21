2a259f6cc1d68b32cf6982c5d796a39e1c91e8cd
이동호 <idongho@idonghoui-MacBook-Pro.local>
2016-04-21 22:02:54 +0900

    added blog app

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    uptated_at = models.DateTimeField(auto_now=True)
+
